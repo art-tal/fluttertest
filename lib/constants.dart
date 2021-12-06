93414272ebda01d2be0309d13c73a33af7dfcc13
art-tal <talanov-artem@ukr.net>
2021-12-06 12:26:37 +0200

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color colorMain = Color.fromARGB(0xFF, 0x31, 0x43, 0xAA);
+const Color colorFont = Color.fromARGB(0xFF, 0x00, 0x00, 0x00);
+const Color colorBackground = Color.fromARGB(0xFF, 0xE5, 0xE5, 0xE5);
+const String imageUrl = 'assets/images/bubbles.png';
